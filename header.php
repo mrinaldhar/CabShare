cc1647732e3ffb5d26544039a0c68d84da72f40c
Mrinal Dhar <mrinal.dhar@gmail.com>
2015-12-05 15:34:56 +0530

    Different headers based on auth status

--- header.php
+++ header.php
@@ -1,21 +1,42 @@
 <?php
+require_once("./utils/ldap.php");
+require_once("./utils/userhelper.php");
+	if (isLoggedIn()) {
+?>
+		<table class="header">
+			<tr>
+				<td width="30%">
+					<button class="btn anim">+ New trip</button>
+					<button class="btn anim">View current status</button>
+					<button class="btn anim">All other poolers</button>
+				</td>
+				<td width="40%">
+					<span id="logo_top">Cab Share<span id="iiith">@IIIT-H</span></span>
+				</td>
+				<td width="30%">
+					<button class="btn anim">Logout: <?php echo getName(); ?></button>
+				</td>
+			</tr>
+		</table>
+
+<?php
+	}
+	else {
 ?>
 
-<table class="header">
-	<tr>
-		<td width="30%">
-			<button class="btn anim">+ New trip</button>
-			<button class="btn anim">View current status</button>
-			<button class="btn anim">All other poolers</button>
-		</td>
-		<td width="40%">
-<span id="logo_top">Cab Share<span id="iiith">@IIIT-H</span></span>
-		</td>
-		<td width="30%">
-			<button class="btn anim">Logout: Mrinal Dhar</button>
-		</td>
-	</tr>
-</table>
+		<table class="header">
+			<tr>
+				<td width="30%">
+				</td>
+				<td width="40%">
+					<span id="logo_top">Cab Share<span id="iiith">@IIIT-H</span></span>
+				</td>
+				<td width="30%">
+					<button class="btn anim">About</button>
+				</td>
+			</tr>
+		</table>
 
-<?php 
+<?php
+}
 ?>
