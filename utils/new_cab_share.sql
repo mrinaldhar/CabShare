e8f92aeab2961354c3b1446e95af9631e0538ca2
Amogh Pradeep <amoghbl1@gmail.com>
2015-12-06 18:14:03 +0530

    db structure added

---
+++ utils/new_cab_share.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 5.1.73, for redhat-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: cab
+-- ------------------------------------------------------
+-- Server version	5.1.73
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `new_cab_share`
+--
+
+DROP TABLE IF EXISTS `new_cab_share`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `new_cab_share` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `source_addr` varchar(50) NOT NULL,
+  `dest_addr` varchar(50) NOT NULL,
+  `date` varchar(50) NOT NULL,
+  `time` varchar(50) NOT NULL,
+  `travellers` varchar(50) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `comment` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `new_cab_share`
+--
+
+LOCK TABLES `new_cab_share` WRITE;
+/*!40000 ALTER TABLE `new_cab_share` DISABLE KEYS */;
+INSERT INTO `new_cab_share` VALUES (1,'iiit','airport','today','now','1','teehee',''),(2,'iiit','airport','today','now','1','',''),(3,'test','test','test','test','test','test',''),(4,'test','test','test','test','test','test',''),(5,'test','test','test','test','test','test',''),(6,'test','test','test','test','test','test',''),(7,'test','test','test','test','test','test',''),(8,'test','test','test','test','test','test',''),(9,'test','test','test','test','test','test','test'),(10,'test','test','test','test','test','test','test'),(11,'test','test','test','test','test','test','test');
+/*!40000 ALTER TABLE `new_cab_share` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-12-06 18:09:28
