8744d8a85c1783b446cefe4cd4757d8fb9eef13c
Mrinal Dhar <mrinal.dhar@gmail.com>
2015-12-17 00:17:40 +0530

    Modifying DB to include long addresses

--- utils/new_cab_share.sql
+++ utils/new_cab_share.sql
@@ -1,60 +1,74 @@
--- MySQL dump 10.13  Distrib 5.1.73, for redhat-linux-gnu (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.2.1
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: cab
--- ------------------------------------------------------
--- Server version	5.1.73
+-- Host: localhost
+-- Generation Time: Dec 16, 2015 at 06:46 PM
+-- Server version: 5.6.20
+-- PHP Version: 5.5.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `cab`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `new_cab_share`
 --
 
-DROP TABLE IF EXISTS `new_cab_share`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `new_cab_share` (
-  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `new_cab_share` (
+`id` int(6) unsigned NOT NULL,
   `userid` varchar(50) NOT NULL,
-  `source_addr` varchar(50) NOT NULL,
-  `dest_addr` varchar(50) NOT NULL,
+  `source_addr` longtext NOT NULL,
+  `dest_addr` longtext NOT NULL,
   `date` varchar(50) NOT NULL,
   `start_time` varchar(50) NOT NULL,
   `end_time` varchar(50) NOT NULL,
   `travellers` varchar(50) NOT NULL,
   `phone` varchar(50) NOT NULL,
   `comment` varchar(50) NOT NULL,
-  `state` int(6) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `state` int(6) unsigned NOT NULL
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 --
 -- Dumping data for table `new_cab_share`
 --
 
-LOCK TABLES `new_cab_share` WRITE;
-/*!40000 ALTER TABLE `new_cab_share` DISABLE KEYS */;
-INSERT INTO `new_cab_share` VALUES (1,'iiit','airport','today','now','1','teehee',''),(2,'iiit','airport','today','now','1','',''),(3,'test','test','test','test','test','test',''),(4,'test','test','test','test','test','test',''),(5,'test','test','test','test','test','test',''),(6,'test','test','test','test','test','test',''),(7,'test','test','test','test','test','test',''),(8,'test','test','test','test','test','test',''),(9,'test','test','test','test','test','test','test'),(10,'test','test','test','test','test','test','test'),(11,'test','test','test','test','test','test','test');
-/*!40000 ALTER TABLE `new_cab_share` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `new_cab_share` (`id`, `userid`, `source_addr`, `dest_addr`, `date`, `start_time`, `end_time`, `travellers`, `phone`, `comment`, `state`) VALUES
+(12, 'mrinal.dhar', 'IIIT, Gachibowli', 'Inorbit Mall Road, HITEC City', '12/12/2012', '0122', '0212', '0312', '9797682837', 'Hello', 0),
+(13, 'mrinal.dhar@research.iiit.ac.in', 'a', 'n', '12/17/2015', '0900', '1700', '2', '213124', '5', 0),
+(14, 'mrinal.dhar@research.iiit.ac.in', 'a', 'asdasd', '12/24/2015', '0900', '1700', '1', '1231231231', 'we', 0),
+(15, 'mrinal.dhar@research.iiit.ac.in', 'IIIT, Gachibowli', 'Inorbit Mall Road', '12/18/2015', '0809', '1605', '2', '1231231232', '2 bags', 0);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `new_cab_share`
+--
+ALTER TABLE `new_cab_share`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `new_cab_share`
+--
+ALTER TABLE `new_cab_share`
+MODIFY `id` int(6) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2015-12-06 18:09:28
