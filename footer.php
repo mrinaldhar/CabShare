040c27f1e6f823a1e5775456241f2d696bfe2d47
Mrinal Dhar <mrinal.dhar@gmail.com>
2016-02-01 01:43:43 +0530

    CHANGING THE BLUES

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <div class="footer">
-Designed by <a href="http://mrinaldhar.com" class="anim">Mrinal Dhar</a> &amp; Backend by <a href="http://amoghbl1.me" class="anim">Amogh Pradeep</a>
+Developed by <a href="http://mrinaldhar.com" class="anim">Mrinal Dhar</a> &bull; Backend by <a href="http://amoghbl1.me" class="anim">Amogh Pradeep</a>
 </div>
