ec6a9fa4473f1d32e9bd7327626cc44e40b1fd31
Mrinal Dhar <mrinal.dhar@gmail.com>
2016-03-21 12:35:56 +0530

    spam protection

--- login.php
+++ login.php
@@ -7,6 +7,10 @@ $username = $_POST['username']; // TODO: Escape this properly.
 $password = $_POST['password'];
 $response = login($username, $password);
 if ($response == true) {
+	$logfile = fopen("./logs/userlog.txt", "a");
+ 	$txt = $_SERVER['REMOTE_ADDR'] . " " . $username . "\n";
+	fwrite($logfile, $txt);
+	fclose($logfile);
 	$home_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/home.php';                                 // the user is redirected to the home page if logged in
 	header('Location: ' . $home_url); 
 }
